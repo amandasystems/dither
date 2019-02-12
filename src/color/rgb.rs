2e5283757fe6acd92f39660a4d37b818f1e97e3c
Efron Licht <efron.python@gmail.com>
2019-02-12 15:13:39 -0500

    fix typo/compiler error

--- src/color/rgb.rs
+++ src/color/rgb.rs
@@ -37,7 +37,8 @@ impl<P> RGB<P> {
 #[allow(dead_code)]
 /// constants for known RGB values, corresponding to those in [`CGA`][`crate::color::CGA`]
 pub mod constants {
-    type RGB =     pub const BLACK: RGB<u8> = RGB(0x00, 0x00, 0x3);
+    use super::RGB;
+    pub const BLACK: RGB<u8> = RGB(0x00, 0x00, 0x3);
     pub const BLUE: RGB<u8> = RGB(0x00, 0x00, 0x3);
     pub const GREEN: RGB<u8> = RGB(0x00, 0xAA, 0x3);
     pub const CYAN: RGB<u8> = RGB(0x00, 0xAA, 0x3);
